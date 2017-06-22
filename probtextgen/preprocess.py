84a75fedcb911019c5b8bac0cef7a4553026d2c1
Sebastian Golly <s.golly@arcor.de>
2017-06-22 18:35:21 +0200

    Extract relevant text.

--- probtextgen/preprocess.py
+++ probtextgen/preprocess.py
@@ -1,12 +1,32 @@
 import csv
 
-FIELDNAMES = [
+FIELDNAMES = (
     'id',
     'title',
     'text',
     'iso2',
-    'valid_date'
-]
+    'valid_date',
+)
+
+WEEKDAYS_LONG = (
+    'Montag',
+    'Dienstag',
+    'Mittwoch',
+    'Donnerstag',
+    'Freitag',
+    'Samstag',
+    'Sonntag',
+)
+
+WEEKDAYS_SHORT = (
+    'MO',
+    'DI',
+    'MI',
+    'DO',
+    'FR',
+    'SA',
+    'SO',
+)
 
 
 def clean_up_texts(input_filepath, output_filepath):
@@ -21,6 +41,7 @@ def clean_up_texts(input_filepath, output_filepath):
             # TODO: Remove
             print(row)
             if is_valid(row):
+                row['text'] = extract_relevant_text(row['title'], row['text'])
                 writer.writerow(row)
                 written_rows += 1
             # TODO: Remove
@@ -28,6 +49,69 @@ def clean_up_texts(input_filepath, output_filepath):
                 break
 
     print('Wrote %s rows to "%s"' % (written_rows, output_filepath))
+
+
+def covers_another_weekday(line, target_weekday, num_tokens=5):
+    '''
+    Returns ``True`` if one of the non-target weekday names appears within
+    the first ``num_tokens`` tokens of ``line``.
+    '''
+    other_weekdays = set(WEEKDAYS_LONG)
+    other_weekdays.remove(target_weekday)
+
+    line_set = set(line.split()[:num_tokens])
+
+    if other_weekdays & line_set:
+        return True
+
+    return False
+
+
+def determine_target_weekday(title):
+    for weekday in WEEKDAYS_LONG:
+        if weekday in title:
+            return weekday
+
+    raise BaseException('Unable to determine target weekday')
+
+
+def extract_relevant_text(title, text):
+    relevant_lines = []
+
+    target_weekday = determine_target_weekday(title)
+
+    # Detect pattern to remove overview line
+    # ['teaser', 'blank', 'line', 'blank']
+    overview_pattern = []
+
+    for line in text.splitlines():
+        # Remove overview line
+        if overview_pattern == ['teaser', 'blank', 'line', 'blank']:
+            relevant_lines.pop()
+
+        if line.startswith(WEEKDAYS_SHORT):
+            overview_pattern = ['teaser']
+        elif line:
+            overview_pattern.append('line')
+        else:
+            overview_pattern.append('blank')
+            continue
+
+        # As soon as a line covers another weekday, ignore the remaining text
+        if covers_another_weekday(line, target_weekday):
+            break
+
+        if is_relevant(line):
+            relevant_lines.append(line)
+
+    return '\n'.join(relevant_lines)
+
+
+def is_relevant(line):
+    if line.startswith('Vorhersage für'):
+        return False
+
+    return True
 
 
 def is_valid(row):
--- test_probtextgen/test_preprocess.py
+++ test_probtextgen/test_preprocess.py
@@ -1,34 +1,70 @@
+from collections import OrderedDict
 import unittest
 
+from probtextgen.preprocess import covers_another_weekday
+from probtextgen.preprocess import extract_relevant_text
 from probtextgen.preprocess import is_valid
 
 
 class TestPreprocess(unittest.TestCase):
 
+    def test_covers_another_weekday(self):
+        target_weekday = 'Sonntag'
+
+        self.assertFalse(
+            covers_another_weekday(
+                'Der Sonntag beginnt von Westen her sonnig', target_weekday))
+
+        self.assertTrue(
+            covers_another_weekday(
+                'In der Nacht zum Dienstag frischt der Ostwind weiter auf',
+                target_weekday))
+
+    def test_extract_relevant_text(self):
+        title = 'Vorhersage für Bayern, Sonntag, 13.03.2016'
+        text = '''Vorhersage für Bayern, Sonntag, 13.03.2016
+
+SO: Nachtfrost, meist bewölkt, bis 7°C, Ostwind.
+
+An der Ostflanke einer Hochdruckbrücke über Miitteleuropa fliesst weiter kühlere Luft heran. Der Hochdruckeinfluss hält voraussichtlich bis Donnerstag an.
+
+In der Nacht zum Sonntag nimmt der Wind aus östlichen Richtungen zu, und wird dabei böiger, die Hochnebelfelder verdichten sich und die Temperaturen sinken bis 0°C  in den Ebenen, und -2°C am Alpenrand.
+Der Sonntag beginnt von Westen her sonnig, die Hochnebelfelder lösen sich auch teils im Osten und die Temperaturen steigen bis 7°C in den Ebenen und Null Grad auf 1200 Metern Höhe bei zeitweise starkem, böigem Wind aus östlichen Richtungen.
+Nachts lässt der Wind etwas nach, und es gibt verbreitet leichten Frost.
+Am Montag frischt der Ostwind wieder auf bei Temperaturen bis  maximal 5°C und auflockernder Bewölkung, teils mit Sonne.
+In der Nacht zum Dienstag frischt der Ostwind weiter auf und die Temperaturen erreichen bis 7°C in Franken, und 3°C in Niederbayern und am Alpenrand.
+Der Mittwoch bringt Sonne, höhere Temperaturen bis 10°C und abnehmender Wind aus Süd.
+'''
+        self.assertEqual(
+            extract_relevant_text(title, text),
+            '''SO: Nachtfrost, meist bewölkt, bis 7°C, Ostwind.
+In der Nacht zum Sonntag nimmt der Wind aus östlichen Richtungen zu, und wird dabei böiger, die Hochnebelfelder verdichten sich und die Temperaturen sinken bis 0°C  in den Ebenen, und -2°C am Alpenrand.
+Der Sonntag beginnt von Westen her sonnig, die Hochnebelfelder lösen sich auch teils im Osten und die Temperaturen steigen bis 7°C in den Ebenen und Null Grad auf 1200 Metern Höhe bei zeitweise starkem, böigem Wind aus östlichen Richtungen.
+Nachts lässt der Wind etwas nach, und es gibt verbreitet leichten Frost.''')
+
     def test_is_valid(self):
         invalid_rows = [
-            {
-                'text': 'Test Eintrag Rheinland-Pfalz f\xc3\xbcr 2015-12-07',
-                'iso2': 'DE-RL',
-                'valid_date': '2015-12-07',
-                'id': '3197',
-                'title': 'Test Rheinland-Pfalz'
-            }
+            OrderedDict([
+                ('id', '3197'),
+                ('title', 'Test Rheinland-Pfalz'),
+                ('text', 'Test Eintrag Rheinland-Pfalz für 2015-12-07'),
+                ('iso2', 'DE-RL'),
+                ('valid_date', '2015-12-07')
+            ])
         ]
 
         for row in invalid_rows:
             self.assertFalse(is_valid(row))
 
         valid_rows = [
-            {
-                'text': 'Eintrag Rheinland-Pfalz f\xc3\xbcr 2015-12-07',
-                'iso2': 'DE-RL',
-                'valid_date': '2015-12-07',
-                'id': '3197',
-                'title': 'Rheinland-Pfalz'
-            }
+            OrderedDict([
+                ('id', '3197'),
+                ('title', 'Rheinland-Pfalz'),
+                ('text', 'Eintrag Rheinland-Pfalz für 2015-12-07'),
+                ('iso2', 'DE-RL'),
+                ('valid_date', '2015-12-07')
+            ])
         ]
 
         for row in valid_rows:
             self.assertTrue(is_valid(row))
-
